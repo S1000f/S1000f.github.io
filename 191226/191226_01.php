a7bb5e46ed3235081a8c3dea530044927dbde71c
Dohyeop Kim <59071967+S1000f@users.noreply.github.com>
2020-03-05 18:05:13 +0900

    Add files via upload

---
+++ 191226/191226_01.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+    191226_01.php
+
+    <?php
+    $aaa = $_GET['aaa'];
+    $bbb = $_GET['bbb'];
+
+    echo "<br />";
+    echo '$aaa = '.$aaa."<br />";
+    echo '$bbb = '.$bbb."<br />";
+    ?>
+
+</body>
+</html>
